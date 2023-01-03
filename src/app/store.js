edf9cd9a9338659e00db527fa3a5c23d26c7703a
Jaydeep.bhaktariya <jaydeep.n@agileinfoways.com>
2023-01-03 10:40:39 +0530

    Cart APP

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/CounterSlice";
+import productReducer from "../features/product/ProductSlicer";
+import { productAPI } from "../features/product/ProductAPI";
+
+import cartReducer from "../features/product/CartSlicer";
+
+// import { applyMiddleware, combineReducers, createStore } from "redux";
+// import Raven from "react-raven";
 
 export const store = configureStore({
   reducer: {
